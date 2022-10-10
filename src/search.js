8e1f46f3eca681bbf05f6ffde96145668da520f6
Armaan <armaaanpal25@gmail.com>
2022-10-10 17:38:53 -0400

    added proxy in package.json

--- src/search.js
+++ src/search.js
@@ -23,7 +23,7 @@ function Search() {
 
   async function getAnimal() {
     const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+      `/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
     const json = await res.json();
     setPets(json.pets);
